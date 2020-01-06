8c9ae6f9164f56b7f5d9325b6feb06f2f99310f2
mattosaurus <mattosaurus@outlook.com>
2020-01-06 14:34:02 +0000

    Add sql to cloud storage example

---
+++ DataTransfer.SqlToGoogleCloudStorage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataTransfer.SqlToGoogleCloudStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
